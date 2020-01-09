7586486fa494a13949d07b3c2e257a9ea0ac7067
Sebastian Ankargren <sebastian.ankargren@statistics.uu.se>
2020-01-09 12:40:02 +0000

    version 0.5.2

--- inst/doc/mfbvar_jss.R
+++ inst/doc/mfbvar_jss.R
@@ -1,5 +1,6 @@
 ## ----preliminaries, echo=FALSE--------------------------------------
 options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
+run_mod <- FALSE
 
 ## ----setup, include=FALSE, cache=FALSE------------------------------
 library(knitr)
@@ -14,7 +15,9 @@ local({
 set.seed(100)
 
 ## ----message = FALSE------------------------------------------------
-library("tidyverse")
+library("purrr")
+library("dplyr")
+library("ggplot2")
 library("alfred")
 
 variables <- c("CPIAUCSL", "UNRATE", "GDPC1")
@@ -67,34 +70,23 @@ prior <- update_prior(prior, n_fcst = 24)
 ## -------------------------------------------------------------------
 summary(prior)
 
-## ----mod_chunk------------------------------------------------------
-mod_ss_iw  <- estimate_mfbvar(prior, prior = "ss", variance = "iw")
-mod_ssng_iw <- estimate_mfbvar(prior, prior = "ssng", variance = "iw")
-
-
-## ----mod_chunk2-----------------------------------------------------
-mod_ss_csv  <- estimate_mfbvar(prior, prior = "ss", variance = "csv")
-mod_ss_fsv <- estimate_mfbvar(prior, prior = "ss", variance = "fsv",
+## ----comp_chunk,include=FALSE---------------------------------------
+if (run_mod) {
+  mod_ss_iw  <- estimate_mfbvar(prior, prior = "ss", variance = "iw")
+  mod_ssng_iw <- estimate_mfbvar(prior, prior = "ssng", variance = "iw")
+  mod_ss_csv  <- estimate_mfbvar(prior, prior = "ss", variance = "csv")
+  mod_ss_fsv <- estimate_mfbvar(prior, prior = "ss", variance = "fsv",
                               n_fac = 1)
 
-
-## -------------------------------------------------------------------
-predict(mod_ss_iw, pred_bands = 0.8)
-
-## ----ss_plots, fig.cap = "Forecasts and posterior steady-state intervals", fig.subcap= c("Steady-state prior", "Hierarchical steady-state prior"), out.width='0.49\\linewidth', fig.width = 5.5, fig.asp = 1.5----
-plot(mod_ss_iw, plot_start = "2010-01-01", nrow_facet = 3)
-plot(mod_ssng_iw, plot_start = "2010-01-01", nrow_facet = 3)
-
-## ----ridges, fig.asp = 0.65, message = FALSE, fig.cap = "Distributions of forecasts produced using the steady-state prior with constant or time-varying error covariance. The vertical lines represent the medians."----
-pred_iw <- predict(mod_ss_iw, pred_bands = NULL)
-pred_csv <- predict(mod_ss_csv, pred_bands = NULL)
-pred_fsv <- predict(mod_ss_fsv, pred_bands = NULL)
-pred_df <- bind_rows("Inverse Wishart" = pred_iw,
-                     "Common stochastic volatility" = pred_csv,
-                     "Factor stochastic volatility" = pred_fsv,
-                     .id = "Variance") %>%
-  filter(variable == "GDPC1")
-ggplot(pred_df, aes(y = factor(fcst_date), x = fcst, fill = Variance)) +
+  predict_example <- predict(mod_ss_iw, pred_bands = 0.8)
+  p1 <- plot(mod_ss_iw, plot_start = "2010-01-01", nrow_facet = 3)
+  p2 <- plot(mod_ssng_iw, plot_start = "2010-01-01", nrow_facet = 3)
+  pred_df <- bind_rows("Inverse Wishart" = predict(mod_ss_iw, pred_bands = NULL),
+                       "Common stochastic volatility" = predict(mod_ss_csv, pred_bands = NULL),
+                       "Factor stochastic volatility" = predict(mod_ss_fsv, pred_bands = NULL),
+                       .id = "Variance") %>%
+    filter(variable == "GDPC1")
+  p3 <- ggplot(pred_df, aes(y = factor(fcst_date), x = fcst, fill = Variance)) +
   ggridges::stat_density_ridges(quantile_lines = TRUE,
                                 quantiles = 2, alpha = 0.5) +
   labs(x = "US GDP Growth",
@@ -103,40 +95,108 @@ ggplot(pred_df, aes(y = factor(fcst_date), x = fcst, fill = Variance)) +
   theme_minimal() +
   scale_fill_brewer(palette = "YlGnBu")
 
-## ----varplot, fig.asp = 0.5, fig.cap = "Standard deviation of the error term in the equation for GDP growth. The black solid and red dashed lines are the medians from the models with factor stochastic and constant volatility, respectively. The bands are obtained from the 95 \\% posterior point-wise intervals.", cache = TRUE, fig.subcap= c("Factor stochastic volatility", "Common stochastic volatility"), out.width='0.49\\linewidth', fig.width = 5----
-const_vol <- median(sqrt(mod_ss_iw$Sigma[3, 3, ]))
+  const_vol <- median(sqrt(mod_ss_iw$Sigma[3, 3, ]))
 
-varplot(mod_ss_fsv, variables = "GDPC1") +
-  geom_hline(yintercept = const_vol ,
-             color = "red", linetype = "dashed") +
-  coord_cartesian(ylim = c(0, 20))
+  p4 <- varplot(mod_ss_fsv, variables = "GDPC1") +
+    geom_hline(yintercept = const_vol ,
+               color = "red", linetype = "dashed") +
+    coord_cartesian(ylim = c(0, 20))
 
-varplot(mod_ss_csv, variables = "GDPC1") +
-  geom_hline(yintercept = const_vol,
-             color = "red", linetype = "dashed") +
-  coord_cartesian(ylim = c(0, 20))
+  p5 <- varplot(mod_ss_csv, variables = "GDPC1") +
+    geom_hline(yintercept = const_vol,
+               color = "red", linetype = "dashed") +
+    coord_cartesian(ylim = c(0, 20))
 
-## -------------------------------------------------------------------
-mdd(mod_ss_iw)
+  mdd_example <- mdd(mod_ss_iw)
 
-## ----par_chunk, include = FALSE-------------------------------------
-library("parallel")
-par_fun <- function(lambda1, prior) {
-  set.seed(2019)
-  mod_par <- estimate_mfbvar(prior, prior = "ss", variance = "iw",
-                             lambda1 = lambda1, lambda3 = 1)
-  mdd(mod_par)
+  library("parallel")
+  par_fun <- function(lambda1, prior) {
+    set.seed(2019)
+    mod_par <- estimate_mfbvar(prior, prior = "ss", variance = "iw",
+                               lambda1 = lambda1, lambda3 = 1)
+    mdd(mod_par)
+  }
+
+  cl <- makeCluster(2)
+  clusterEvalQ(cl, library("mfbvar"))
+  lambda1_seq <- seq(0.05, 1, by = 0.05)
+  result <- parSapply(cl, lambda1_seq,
+                      par_fun, prior = prior)
+  stopCluster(cl)
+  max_val <- tibble(lambda1 = lambda1_seq,
+         mdd = result) %>%
+    filter(mdd == max(mdd)) %>% .$lambda1
+
+
+  save(predict_example, mdd_example, lambda1_seq, result, max_val,
+       file = "vignettes/vignette_data.RData",
+       compress = "xz")
+  ggsave("vignettes/figures/ss_plots-1.pdf", p1, "pdf", width = 5.5, height = 5.5*1.5, units = "in")
+  ggsave("vignettes/figures/ss_plots-2.pdf", p2, "pdf", width = 5.5, height = 5.5*1.5, units = "in")
+  ggsave("vignettes/figures/ridges-1.pdf", p3, "pdf", width = 7, height = 7*0.65, units = "in")
+  ggsave("vignettes/figures/varplot-1.pdf", p4, "pdf", width = 5, height = 5*0.5, units = "in")
+  ggsave("vignettes/figures/varplot-2.pdf", p5, "pdf", width = 5, height = 5*0.5, units = "in")
+} else {
+  load("vignette_data.RData")
 }
 
-cl <- makeCluster(2)
-clusterEvalQ(cl, library("mfbvar"))
-lambda1_seq <- seq(0.05, 1, by = 0.05)
-result <- parSapply(cl, lambda1_seq,
-                    par_fun, prior = prior)
-stopCluster(cl)
+## ----mod_chunk,eval=FALSE-------------------------------------------
+#  mod_ss_iw  <- estimate_mfbvar(prior, prior = "ss", variance = "iw")
+#  mod_ssng_iw <- estimate_mfbvar(prior, prior = "ssng", variance = "iw")
+
+## ----mod_chunk2,eval=FALSE------------------------------------------
+#  mod_ss_csv  <- estimate_mfbvar(prior, prior = "ss", variance = "csv")
+#  mod_ss_fsv <- estimate_mfbvar(prior, prior = "ss", variance = "fsv",
+#                                n_fac = 1)
+
+## ----eval=FALSE-----------------------------------------------------
+#  predict(mod_ss_iw, pred_bands = 0.8)
+
+## ----echo=FALSE-----------------------------------------------------
+predict_example
+
+## ----eval=FALSE-----------------------------------------------------
+#  plot(mod_ss_iw, plot_start = "2010-01-01", nrow_facet = 3)
+#  plot(mod_ssng_iw, plot_start = "2010-01-01", nrow_facet = 3)
+
+## ----eval=FALSE-----------------------------------------------------
+#  pred_iw <- predict(mod_ss_iw, pred_bands = NULL)
+#  pred_csv <- predict(mod_ss_csv, pred_bands = NULL)
+#  pred_fsv <- predict(mod_ss_fsv, pred_bands = NULL)
+#  pred_df <- bind_rows("Inverse Wishart" = pred_iw,
+#                       "Common stochastic volatility" = pred_csv,
+#                       "Factor stochastic volatility" = pred_fsv,
+#                       .id = "Variance") %>%
+#    filter(variable == "GDPC1")
+#  ggplot(pred_df, aes(y = factor(fcst_date), x = fcst, fill = Variance)) +
+#    ggridges::stat_density_ridges(quantile_lines = TRUE,
+#                                  quantiles = 2, alpha = 0.5) +
+#    labs(x = "US GDP Growth",
+#         y = "Date of Forecast") +
+#    coord_cartesian(xlim = c(-5, 10)) +
+#    theme_minimal() +
+#    scale_fill_brewer(palette = "YlGnBu")
+
+## ----eval=FALSE-----------------------------------------------------
+#  const_vol <- median(sqrt(mod_ss_iw$Sigma[3, 3, ]))
+#  
+#  varplot(mod_ss_fsv, variables = "GDPC1") +
+#    geom_hline(yintercept = const_vol ,
+#               color = "red", linetype = "dashed") +
+#    coord_cartesian(ylim = c(0, 20))
+#  
+#  varplot(mod_ss_csv, variables = "GDPC1") +
+#    geom_hline(yintercept = const_vol,
+#               color = "red", linetype = "dashed") +
+#    coord_cartesian(ylim = c(0, 20))
+
+## ----eval=FALSE-----------------------------------------------------
+#  mdd(mod_ss_iw)
+
+## ----echo=FALSE-----------------------------------------------------
+mdd_example
 
 ## ----par_chunk2, eval = FALSE---------------------------------------
-#  
 #  library("parallel")
 #  par_fun <- function(lambda1, prior) {
 #    set.seed(2019)
@@ -151,11 +211,6 @@ stopCluster(cl)
 #  result <- parSapply(cl, lambda1_seq,
 #                      par_fun, prior = prior)
 #  stopCluster(cl)
-
-## ----include=FALSE--------------------------------------------------
-max_val <- tibble(lambda1 = lambda1_seq,
-       mdd = result) %>%
-  filter(mdd == max(mdd)) %>% .$lambda1
 
 ## ----mdd_plot,fig.asp = 0.4, fig.cap = sprintf("Logarithm of the marginal data density as a function of $\\lambda_1$ with $\\lambda_3=1$. The point shows the maximum point at $\\lambda_1=%4.1f$.", max_val)----
 plot_df <- tibble(lambda1 = lambda1_seq,
